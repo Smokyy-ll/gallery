d250a3c68192595e1fd2139d6f9690f0aa574aa3
Kirill Magonov <magonov09@bk.ru>
2025-04-01 19:39:13 +0300

    feat: added class form

---
+++ src/components/form.js
@@ -0,0 +1,32 @@
+import Creator from "../utilities/creator";
+
+const formParams = {
+    tagName: 'form',
+    classList: ['first', 'second'], 
+}
+
+const inputParams = {
+    tagName: 'input',
+    classList: ['first', 'second'], 
+}
+
+const buttonParams = {
+    tagName: 'button',
+    text: 'Отправить',
+    classList: ['first', 'second'], 
+}
+
+class Form extends Creator {
+    constructor(){
+        super(formParams)
+        this.input = new Creator(inputParams)
+        this.button = new Creator(buttonParams)
+        this.addChildren()
+    }
+    addChildren(){
+        this.element.append(this.input.getElement())
+        this.element.append(this.button.getElement())
+    }
+}
+
+export default Form
--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,20 @@
 import Creator from "../utilities/creator.js"
+import Form from "./form.js"
 
 const headerParams = {
     tagName: 'header',
     classList: ['first', 'second'], 
-    text: 'header',
-    attr: {
-        id: 'headerId',
-        'data': 'header'
-    } 
 }
 
 class Header extends Creator {
     constructor(){
         super(headerParams)
+        this.form = new Form()
+        this.addChildren()
+    }
+
+    addChildren(){
+        this.element.append(this.form.getElement())
     }
 }
 
